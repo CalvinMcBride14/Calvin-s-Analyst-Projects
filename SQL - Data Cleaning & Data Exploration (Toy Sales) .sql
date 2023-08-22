c367aac1a5cf07d4b1a839380d544b520fdb44ea
Calvin McBride <135881962+CalvinMcBride14@users.noreply.github.com>
2023-08-22 15:35:52 -0500

    SQL - Data Cleaning & Data Exploration (Toy Sales) .sql

--- SQL - Data Cleaning & Data Exploration (Toy Sales) .sql
+++ SQL - Data Cleaning & Data Exploration (Toy Sales) .sql
@@ -17,29 +17,29 @@ Mexico toys company is a fictitious company and data are provided by Maven Analy
 /* Convert Date columns to the appropriate data type. */
 
 ALTER table sales
-   modify Date DATE;
+   modify date DATE;
    
 ALTER table stores
-   modify Store_Open_Date DATE;
+   modify store_open_date DATE;
    
 ----------------------------------------------------------------------------------------------------------------------------------------------------
 /* Product_cost and product_price columns contain '$' remove this special character from all rows. */ 
 
  UPDATE  products
-   SET Product_Cost = REPLACE(Product_Cost,'$',' ');
+   SET product_cost = REPLACE(product_cost,'$',' ');
    
    UPDATE  products
-   SET Product_Price = REPLACE(Product_Price,'$',' ');
+   SET product_price = REPLACE(product_price,'$',' ');
    
    
  ----------------------------------------------------------------------------------------------------------------------------------------------------  
    /* Now convert these colunns into a decimal data type. */
    
    ALTER TABLE products
-   MODIFY Product_Price DECIMAL(10,2);
+   MODIFY product_price DECIMAL(10,2);
    
    ALTER TABLE products
-   MODIFY Product_Cost DECIMAL(10,2);
+   MODIFY product_cost DECIMAL(10,2);
    
    
 ----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -58,30 +58,30 @@ HAVING ID_appearance > 1;
 
 SELECT  * 
 FROM sales
-WHERE (Sale_ID OR Date OR Store_ID OR Product_ID OR Units) IS NULL;
+WHERE (sale_ID OR date OR store_ID OR product_ID OR units) IS NULL;
 
 
 /* Perfrom Data Exploration */
 ------------------------------
 
-/* Find the MIN, MAX, & AVG for stock, highest, sales price. */ 
+/* Find the MIN, MAX, & AVG for stock, sales price, and cost. */ 
 
 SELECT 
-     MAX(Stock_On_Hand) AS Highest_stock,
-     MIN(Stock_On_Hand) AS Lowest_stock,
-     AVG(Stock_On_Hand) AS Average_stock
+     MAX(stock_on_hand) AS highest_stock,
+     MIN(stock_on_hand) AS lowest_stock,
+     AVG(Stock_on_hand) AS average_stock
 FROM inventory;
 
 SELECT 
-     MAX(Product_Price) AS Highest_sale_price,
-     MIN(Product_Price) AS Lowest_sale_price,
-     AVG(Product_Price) AS Average__sale_price
+     MAX(product_price) AS highest_sale_price,
+     MIN(product_price) AS lowest_sale_price,
+     AVG(product_Price) AS average__sale_price
 FROM products;
 
 SELECT 
-     MAX(Product_Cost) AS Highest_Product_Cost,
-     MIN(Product_Cost) AS Lowest_Product_Cost,
-     AVG(Product_Cost) AS Average__Product_Cost
+     MAX(product_cost) AS highest_product_cost,
+     MIN(product_cost) AS lowest_product_cost,
+     AVG(product_cost) AS average__product_cost
 FROM products;
 
 
@@ -89,28 +89,28 @@ FROM products;
 /* Find the Sales Units per products for the top five products. */
 
 SELECT
-     s.Product_ID AS product_ID,
-     p.Product_Name AS name,
-     SUM(s.units) AS Units_sold
+     s.product_ID AS prod_ID,
+     p.product_name AS name,
+     SUM(s.units) AS units_sold
 FROM sales s
      INNER JOIN products p
      ON s.product_ID = p.product_ID
-GROUP BY s.Product_ID, p.Product_Name
-ORDER BY Units_sold DESC  LIMIT 5;
+GROUP BY s.product_ID, p.product_name
+ORDER BY units_sold DESC LIMIT 5;
 
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------
 /* Find the Revenue per product for the top five products. */
 SELECT
-     s.Product_ID AS product_ID,
-     p.Product_Name AS name,
-     p.Product_Price,
-     SUM(s.units) AS Units_sold,
-     p.Product_Price * SUM(s.units) AS revenue_per_product
+     s.product_ID AS product_ID,
+     p.product_name AS name,
+     p.product_price,
+     SUM(s.units) AS units_sold,
+     p.product_price * SUM(s.units) AS revenue_per_product
 FROM sales s
      INNER JOIN products p
      ON s.product_ID = p.product_ID
-GROUP BY s.Product_ID, p.Product_Name, p.Product_Price
+GROUP BY s.Product_ID, p.product_name, p.product_price
 ORDER BY revenue_per_product DESC LIMIT 5;
 
 
@@ -118,15 +118,15 @@ ORDER BY revenue_per_product DESC LIMIT 5;
 /* Find how much money Mexico Toys spends on their products. */ 
 
 SELECT
-     s.Product_ID AS product_ID,
-     p.Product_Name AS name,
-     p.Product_Cost,
-     SUM(s.units) AS Units_sold,
-     p.Product_Cost * SUM(s.units) AS cost_per_product
+     s.product_ID AS product_ID,
+     p.product_name AS name,
+     p.product_cost,
+     SUM(s.units) AS units_sold,
+     p.product_cost * SUM(s.units) AS cost_per_product
 FROM sales s
      INNER JOIN products p
      ON s.product_ID = p.product_ID
-GROUP BY s.Product_ID, p.Product_Name, p.Product_Cost
+GROUP BY s.product_ID, p.product_name, p.product_cost
 ORDER BY cost_per_product DESC;
 
 
@@ -134,19 +134,19 @@ ORDER BY cost_per_product DESC;
 /* Determine how each product performs by calculating profit and profit percentage per product. */
 
 SELECT
-     s.Product_ID AS product_ID,
-     p.Product_Name AS name,
-     p.Product_Cost,
+     s.product_ID AS product_ID,
+     p.product_name AS name,
+     p.product_cost,
      p.product_price,
-     SUM(s.units) AS Units_sold,
-     SUM(s.units) * p.Product_Cost AS cost_per_product,
-     p.Product_Price * SUM(s.units) AS revenue_per_product,
-     p.Product_Price * SUM(s.units) - p.Product_Cost * SUM(s.units) AS profit,
-     ROUND(((p.Product_Price * SUM(s.units) - p.Product_Cost * SUM(s.units)) / (p.Product_Price * SUM(s.units))) *100,2) AS profit_percentage
+     SUM(s.units) AS units_sold,
+     SUM(s.units) * p.product_Cost AS cost_per_product,
+     p.product_price * SUM(s.units) AS revenue_per_product,
+     p.product_price * SUM(s.units) - p.product_cost * SUM(s.units) AS profit,
+     ROUND(((p.product_price * SUM(s.units) - p.product_Cost * SUM(s.units)) / (p.product_price * SUM(s.units))) *100,2) AS profit_percentage
 FROM sales s
      INNER JOIN products p
      ON s.product_ID = p.product_ID
-GROUP BY s.Product_ID, p.Product_Name, p.Product_Cost, p.product_price
+GROUP BY s.product_ID, p.product_name, p.product_cost, p.product_price
 ORDER BY profit_percentage DESC;
 
 
@@ -154,29 +154,29 @@ ORDER BY profit_percentage DESC;
 /* Find the total cost, total revenue, total profit and profit percentage per type of location to see how the store location can affect strategy. */
 
 SELECT
-     Store_Location,
+     store_location,
      SUM(revenue_per_product) AS revenue,
      SUM(cost_per_product) AS costs,
      SUM(revenue_per_product) - SUM(cost_per_product) AS profit,
      (SUM(revenue_per_product) - SUM(cost_per_product)) / SUM(revenue_per_product) AS profit_percentage
 FROM
      (SELECT
-         st.Store_Location,
-         s.Product_ID AS product_ID,
-         d.Product_Name AS name,
-         d.Product_Price,
-         d.Product_Cost,
-         SUM(s.units) AS Units_sold,
-         d.Product_Price * SUM(s.units) AS revenue_per_product,
-         SUM(s.units) * d.Product_Cost  AS cost_per_product
+         st.store_location,
+         s.product_ID AS product_ID,
+         d.product_name AS name,
+         d.product_price,
+         d.product_cost,
+         SUM(s.units) AS units_sold,
+         d.product_price * SUM(s.units) AS revenue_per_product,
+         SUM(s.units) * d.product_Cost  AS cost_per_product
      FROM sales s
          INNER JOIN products d
          ON s.product_ID = d.product_ID
          INNER JOIN stores st
-         ON st.Store_ID = s.Store_ID
-         GROUP BY st.Store_Location, s.Product_ID, d.Product_Name, d.Product_Price, d.Product_Cost
+         ON st.store_ID = s.store_ID
+         GROUP BY st.store_location, s.product_ID, d.product_name, d.product_price, d.product_cost
          ) AS revenue_and_costs
-GROUP BY Store_Location
+GROUP BY store_location
 ORDER BY profit_percentage DESC;
 
 
@@ -193,19 +193,19 @@ SELECT
      SUM(CASE WHEN Product_Category = 'Sports & Outdoors' THEN revenue_per_product ELSE NULL END) AS Sports_Outdoors_revenue
 FROM
      (SELECT
-          Year(s.Date) AS year,
-          d.Product_Category,
-          s.Product_ID AS product_ID,
-          d.Product_Name AS name,
-          d.Product_Price,
-          d.Product_Cost,
+          Year(s.date) AS year,
+          d.product_category,
+          s.product_ID AS product_ID,
+          d.product_name AS name,
+          d.product_price,
+          d.product_cost,
           SUM(s.units) AS Units_sold,
-          d.Product_Price * SUM(s.units) AS revenue_per_product,
-          SUM(s.units) * d.Product_Cost  AS cost_per_product
+          d.product_price * SUM(s.units) AS revenue_per_product,
+          SUM(s.units) * d.product_Cost  AS cost_per_product
      FROM sales s
           INNER JOIN products d
           ON s.product_ID = d.product_ID
-     GROUP BY Year(s.Date), d.Product_Category, s.Product_ID, d.Product_Name, d.Product_Price, d.Product_Cost
+     GROUP BY Year(s.Date), d.product_category, s.product_ID, d.product_name, d.product_price, d.product_cost
      ) AS revenue_and_costs
 GROUP BY year;
 
@@ -221,17 +221,17 @@ SELECT
      (SUM(revenue_per_product) - SUM(cost_per_product)) / SUM(revenue_per_product) AS profit_percentage
 FROM
     (SELECT 
-        YEAR(s.Date) AS year,
-        s.Product_ID AS product_ID,
-        d.Product_Name AS name,
-        d.Product_Price,
-        d.Product_Cost,
-        SUM(s.units) AS Units_sold,
-        d.Product_Price * SUM(s.units) AS revenue_per_product,
-        SUM(s.units) * d.Product_Cost  AS cost_per_product
+        YEAR(s.date) AS year,
+        s.product_ID AS product_ID,
+        d.product_name AS name,
+        d.product_price,
+        d.product_cost,
+        SUM(s.units) AS units_sold,
+        d.product_price * SUM(s.units) AS revenue_per_product,
+        SUM(s.units) * d.product_Cost  AS cost_per_product
     FROM sales s 
          INNER JOIN products d ON s.product_ID = d.product_ID
-    GROUP BY YEAR(s.Date), s.Product_ID, d.Product_Name, d.Product_Price, d.Product_Cost 
+    GROUP BY YEAR(s.date), s.product_ID, d.product_name, d.product_price, d.product_cost 
     ) AS revenue_and_costs
 GROUP BY year;
 
@@ -243,14 +243,14 @@ SELECT
       SUM(inventory_cost_per_product) AS inv_cost
 FROM
       (SELECT
-         i.Product_ID,
-         p.Product_Name,
-         p.Product_Cost,
-         SUM(i.Stock_On_Hand),
-         SUM(i.Stock_On_Hand) * p.Product_Cost AS inventory_cost_per_product
+         i.product_ID,
+         p.product_name,
+         p.product_cost,
+         SUM(i.stock_on_hand),
+         SUM(i.stock_on_hand) * p.product_cost AS inventory_cost_per_product
      FROM inventory i
          LEFT JOIN products p
-         USING(Product_ID)
-     GROUP BY i.Product_ID, p.Product_Name, p.Product_Cost
+         USING(product_ID)
+     GROUP BY i.product_ID, p.product_name, p.product_cost
      ORDER BY inventory_cost_per_product DESC
      ) AS inv_cost;
